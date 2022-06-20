701d099a50daeb502df8bca9dfcefe535999d18d
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2022-06-20 19:07:07 -0300

    conexão com api imdb

--- apiFilmesDjango/apifilmes/views.py
+++ apiFilmesDjango/apifilmes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from requests import get
 
 # Create your views here.
+BASE_URL = "https://imdb-api.com/"
+MOVIE_URL = "pt-br/API/SearchMovie/"
+
+apiKey = "k_ks2nkhaq/"
+movie = input('Digite o nome do filme: ')
+
+
+def get_movie():
+    response = get(BASE_URL+MOVIE_URL+apiKey+movie).json()
+    return response
+
+
+print(get_movie())
